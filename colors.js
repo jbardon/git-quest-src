9fa5f702f27da3ef0ad42d229e13eecd9440e800
jbardon <bardonjeremy1@gmail.com>
2018-12-08 16:41:13 +0100

    End Quest #2

---
+++ colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    RED: '\x1b[31m',
+    GREEN: '\x1b[32m',
+    YELLOW: '\x1b[33m',
+    MAGENTA: '\x1b[35m',
+    CYAN: '\x1b[36m',
+    WHITE: '\x1b[37m'
+};
+
+module.exports = COLORS;
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const LETTERS = require('./letters');
+const CONSOLE_COLORS = require('./colors');
 
-function addLetterInMessage (messageLines, letterLines) {
-	for (let lineIndex = 0; lineIndex < letterLines.length; lineIndex++) {
-		const currentLines = messageLines[lineIndex] || '';
-		messageLines[lineIndex] = currentLines + letterLines[lineIndex];
-	}
-	return messageLines;
+function addLetterInMessage(messageLines, letterLines) {
+    for (let lineIndex = 0; lineIndex < letterLines.length; lineIndex++) {
+        const currentLines = messageLines[lineIndex] || '';
+        messageLines[lineIndex] = currentLines + letterLines[lineIndex];
+    }
+    return messageLines;
 }
 
 function buildMessage(message) {
-	const lettersLines = message
-		.split('')
-		.map(letter => LETTERS[letter]);
+    const lettersLines = message
+        .toUpperCase()
+        .split('')
+        .map(letter => LETTERS[letter]);
 
-	return lettersLines.reduce(addLetterInMessage, []);
+    return lettersLines.reduce(addLetterInMessage, []);
 }
 
-function printMessage(messageLines) {
-	messageLines.forEach(line => console.log(line));
+function printMessage(messageLines, color = CONSOLE_COLORS.GREEN) {
+    messageLines.forEach(line => console.log(color, line));
 }
 
-function print(message) {
-	printMessage(buildMessage(message));
+function print(message, color) {
+    printMessage(buildMessage(message), color);
 }
 
 print('jbardon');
